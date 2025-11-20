618daf8c60b1eec65f9ad05b4e6936f0c88cd503
Nyantemafy <antema.fy01@gmail.com>
2025-11-20 19:46:22 +0300

    correction index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
+import './index.css';    
+import './styles.css';   
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <HashRouter>
-        <Dashboard />
-        </HashRouter>
-    </React.StrictMode>
-);
+root.render(<App />);
